a4f99323a159b43c0b0effbbfe2b1f54ab4f16f2
sejal99 <sejal8974@gmail.com>
2024-08-02 14:11:29 +0530

    new commit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 export const signup = async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -10,3 +11,24 @@ export const signup = async (req, res) => {
     console.log(error);
   }
 };
+
+export const signin=async(req,res)=>{
+try {
+    const {email,password}=req.body;
+    const loggedUser=await User.findOne({email});
+    if(!loggedUser){
+        return res.json({message:'invalid email or password'})
+    }
+    //check if password matches
+    const matchPassword=await bcrypt.compare(password,loggedUser.password)
+    //create a jwt token
+    
+    const token=jwt.sign({id:loggedUser._id,email:loggedUser.user},process.env.JWT_SECRET,{expiresIn:'1h'});
+    res.json({token})
+    console.log(token);
+    
+} catch (error) {
+    console.log(error);
+    
+}
+}
