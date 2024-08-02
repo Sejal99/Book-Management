77dccc500d081a08e4c1a8c7f7cd5c44d2959e51
sejal99 <sejal8974@gmail.com>
2024-08-02 20:01:09 +0530

    new commit

--- server/models/user.js
+++ server/models/user.js
@@ -19,7 +19,7 @@ const userSchema=new mongoose.Schema({
     },
     role: { 
         type: String, 
-        enum: ['user', 'admin', 'moderator'], 
+        enum: ['user', 'admin'], 
         default: 'user' 
       }
 })
