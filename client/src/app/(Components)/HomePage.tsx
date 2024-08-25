0a30122f7422f9822dd64bf551e159cc1f0e4e48
sejal99 <sejal8974@gmail.com>
2024-08-25 19:49:04 +0530

    new feature added

--- client/src/app/(Components)/HomePage.tsx
+++ client/src/app/(Components)/HomePage.tsx
@@ -1,21 +1,22 @@
-"use client";
+"use client"
 import React, { useCallback, useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Filter from "./Filter";
 import Pagination from "./Pagination";
 import { BASE_URL } from "./base";
-import { AppDispatch, RootState } from "./GlobalRedux/store";
-import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch } from "./GlobalRedux/store";
+import { useDispatch } from "react-redux";
 import { addBook } from "./GlobalRedux/Features/BookSlice";
 import { increment } from "./GlobalRedux/Features/CounterSlice";
-import { Carousel } from "react-responsive-carousel";
 import NextJsCarousel from "./NextJsCarousel";
+import Shimmer from "./Shimmer";
 
 const HomePage = () => {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 15;
   const [getDisable, setDisable] = useState([]);
+  const [loading, setLoading] = useState(true); // Loading state
   const dispatch: AppDispatch = useDispatch();
 
   const fetchData = useCallback(async () => {
@@ -25,32 +26,31 @@ const HomePage = () => {
       });
       const result = await res.json();
       const arr = new Array(result.length).fill(false);
-      // @ts-ignore
       setDisable(arr);
       setData(result);
+      setLoading(false); // Set loading to false once data is loaded
     } catch (error) {
       console.error(error);
+      setLoading(false); // Set loading to false if there's an error
     }
   }, []);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = data?.slice(indexOfFirstItem, indexOfLastItem);
-  //@ts-ignore
+
   const handleAdd = (beer, index) => {
     setDisable((prev) => {
       let newArr = [...prev];
-      // @ts-ignore
       newArr[index] = true;
       return newArr;
     });
 
     dispatch(
-      //@ts-ignore
       addBook({
         bookname: beer.bookname,
         price: beer.price,
@@ -66,63 +66,63 @@ const HomePage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
       <Navbar />
-
-      <NextJsCarousel/>
-      <div className="flex flex-col ">
-
-     
-      <div className="flex  bg-green-800">
-        <div className="w-[15%] p-6 bg-gray-50">
-          <Filter setData={setData} currentItems={currentItems} />
-        </div>
-        <div className="w-[85%] min-h-screen p-6 bg-gray-50">
-          <div className="flex flex-col">
-
-    
-          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
-            {currentItems.map((beer, index) => (
-              <div
-                key={index}
-                className="bg-blue border border-gray-400 shadow-lg rounded-lg overflow-hidden transform transition-transform duration-200 hover:scale-105 flex flex-col h-[320px] w-[75%] mx-auto"
-              >
-                <div className="flex-grow flex justify-center items-center">
-                  <img
-                    // @ts-ignore
-                    src={beer.image}
-                    className="h-40 w-auto object-contain mt-4 text-sm"
-                      // @ts-ignore
-                    alt={beer.bookname}
-                  />
-                </div>
-                <div className="flex-grow flex flex-col justify-between p-2 text-center">
-                  <div>
-                    <h2 className="text-sm font-semibold text-gray-800 mt-4">
-                {/*@ts-ignore*/ }
-                      {beer.bookname}
-                    </h2>
-                    <p className="mt-1 text-sm font-semibold text-gray-800">
-                         {/*@ts-ignore*/ }
-                      ${beer.price}
-                    </p>
-                  </div>
-                  <div className="mt-2">
-                    <button
-                      disabled={getDisable[index]}
-                      onClick={() => handleAdd(beer, index)}
-                      className={`${
-                        getDisable[index] ? "bg-gray-600" : "bg-red-600"
-                      } p-2 rounded-lg text-white text-sm`}
+      <NextJsCarousel />
+      <div className="flex flex-col">
+        <div className="flex bg-green-800">
+          <div className="w-[15%] p-6 bg-gray-50">
+            <Filter setData={setData} currentItems={currentItems} />
+          </div>
+          <div className="w-[85%] min-h-screen p-6 bg-gray-50">
+            <div className="flex flex-col">
+              <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+                {loading ? (
+                  // Render 15 shimmer placeholders while loading
+                  Array(itemsPerPage).fill(<Shimmer />)
+                ) : (
+                  currentItems.map((beer, index) => (
+                    <div
+                      key={index}
+                      className="bg-blue border border-gray-400 shadow-lg rounded-lg overflow-hidden transform transition-transform duration-200 hover:scale-105 flex flex-col h-[320px] w-[75%] mx-auto"
                     >
-                      {getDisable[index] ? "Added to Cart" : "Add To Cart"}
-                    </button>
-                  </div>
-                </div>
+                      <div className="flex-grow flex justify-center items-center">
+                        <img
+                          src={beer.image}
+                          className="h-40 w-auto object-contain mt-4 text-sm"
+                          alt={beer.bookname}
+                        />
+                      </div>
+                      <div className="flex-grow flex flex-col justify-between p-2 text-center">
+                        <div>
+                          <h2 className="text-sm font-semibold text-gray-800 mt-4">
+                            {beer.bookname}
+                          </h2>
+                          <p className="mt-1 text-sm font-semibold text-gray-800">
+                            ${beer.price}
+                          </p>
+                        </div>
+                        <div className="mt-2">
+                          <button
+                            disabled={getDisable[index]}
+                            onClick={() => handleAdd(beer, index)}
+                            className={`${
+                              getDisable[index]
+                                ? "bg-gray-600"
+                                : "bg-red-600"
+                            } p-2 rounded-lg text-white text-sm`}
+                          >
+                            {getDisable[index]
+                              ? "Added to Cart"
+                              : "Add To Cart"}
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                )}
               </div>
-            ))}
-          </div>
+            </div>
           </div>
         </div>
-      </div>
       </div>
       <Pagination
         data={data}
