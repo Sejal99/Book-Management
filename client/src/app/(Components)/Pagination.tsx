1e44e4b4a025f112f04b952a95995ceb3f93fe0c
sejal99 <sejal8974@gmail.com>
2024-08-26 14:12:43 +0530

    new feature added

--- client/src/app/(Components)/Pagination.tsx
+++ client/src/app/(Components)/Pagination.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-//@ts-ignore
+
 const Pagination = ({ data, itemsPerPage, currentPage, setCurrentPage }) => {
   const handlePrev = () => {
     if (currentPage > 1) {
@@ -10,18 +10,23 @@ const Pagination = ({ data, itemsPerPage, currentPage, setCurrentPage }) => {
   const handleNext = () => {
     setCurrentPage(currentPage + 1);
   };
+
   return (
-    <div className="flex justify-center gap-2 pl-14">
+    <div className="flex justify-center items-center gap-4 py-4 pl-14">
       <button
         onClick={handlePrev}
-        className="bg-blue-300 px-2 py-2 rounded-md "
+        className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-blue-600 transition-colors duration-300 disabled:bg-gray-300 disabled:cursor-not-allowed"
+        disabled={currentPage === 1}
       >
         Prev
       </button>
-      <div className=" px-2 py-2">{currentPage}</div>
+      <span className="text-lg font-semibold text-gray-800">
+      {currentPage}
+      </span>
       <button
         onClick={handleNext}
-        className="bg-blue-300 px-2 py-2 rounded-md "
+        className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-blue-600 transition-colors duration-300"
+        disabled={currentPage >= Math.ceil(data.length / itemsPerPage)}
       >
         Next
       </button>
